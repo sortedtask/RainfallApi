43475c4aec29c1af54a1ab7bb24403ddb84db807
Joseph Mortleman <joseph@mortleman.com>
2024-07-07 16:50:04 +0100

    Add swagger boilerplate

--- RainfallApi/Program.cs
+++ RainfallApi/Program.cs
@@ -1,11 +1,23 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Rainfall API",
+        Version = "v1",
+        Contact = new OpenApiContact
+        {
+            Name = "Sorted",
+            Url = new Uri("https://www.sorted.com")
+        }
+    });
+});
 
 var app = builder.Build();
 
@@ -13,11 +25,13 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Rainfall API v1");
+    });
+    app.UseDeveloperExceptionPage();
 }
 
-app.UseAuthorization();
-
+app.UseRouting();
 app.MapControllers();
-
 app.Run();
